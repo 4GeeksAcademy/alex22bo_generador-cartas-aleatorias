4c9986fb566a1a41241e3ce1a463b9c3ab362b43
Luis Alexander Borjas Zamora <119633920+alex22bo@users.noreply.github.com>
2024-11-27 23:25:24 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  const suitClasses = suits.map(suit => `suit-${suit.toLowerCase()}`);
+
+  const randomCardNumbers = Math.floor(Math.random() * cardNumbers.length);
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+
+  document.getElementById("contenidoCarta").innerHTML =
+    cardNumbers[randomCardNumbers];
+  document.getElementById(
+    "theCard"
+  ).className = `card ${suitClasses[randomSuitIndex]}`;
 };
